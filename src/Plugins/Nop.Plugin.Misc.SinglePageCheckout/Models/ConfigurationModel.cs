56608f5324047abc0acd4a5da36e4f85dde1e68d
SateeshMunagala <sateesh.munagala@gmail.com>
2026-06-06 15:23:07 +0530

    checked in error fixes

--- src/Plugins/Nop.Plugin.Misc.SinglePageCheckout/Models/ConfigurationModel.cs
+++ src/Plugins/Nop.Plugin.Misc.SinglePageCheckout/Models/ConfigurationModel.cs
@@ -1,4 +1,4 @@
-using Nop.Web.Framework.Models;
+﻿using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Plugin.Misc.SinglePageCheckout.Models;
@@ -57,4 +57,23 @@ public record ConfigurationModel : BaseNopModel
 
     [NopResourceDisplayName("Plugins.Misc.SinglePageCheckout.Fields.DefaultBillingCountryId")]
     public int DefaultBillingCountryId { get; set; }
+
+    // 2. Add these MISSING Multi-Store Override tracking fields below:
+    public bool Enabled_OverrideForStore { get; set; }
+    public bool BypassCart_OverrideForStore { get; set; }
+    public bool ShowCartOnCheckout_OverrideForStore { get; set; }
+    public bool AllowCartItemEditing_OverrideForStore { get; set; }
+    public bool ShowDiscountBox_OverrideForStore { get; set; }
+    public bool ShowGiftCardBox_OverrideForStore { get; set; }
+    public bool ShowCheckoutAttributes_OverrideForStore { get; set; }
+    public bool ShowOrderReviewData_OverrideForStore { get; set; }
+    public bool ShowEstimateShipping_OverrideForStore { get; set; }
+    public bool EnableBuyNow_OverrideForStore { get; set; }
+    public bool ShowBuyNowOnProductDetails_OverrideForStore { get; set; }
+    public bool ShowBuyNowOnProductBoxes_OverrideForStore { get; set; }
+    public bool PreselectDefaultCustomerAddress_OverrideForStore { get; set; }
+    public bool PreselectLastCustomerBillingAddress_OverrideForStore { get; set; }
+    public bool PreselectLastCustomerShippingAddress_OverrideForStore { get; set; }
+    public bool EnableShipToSameAddressByDefault_OverrideForStore { get; set; }
+    public bool DefaultBillingCountryId_OverrideForStore { get; set; }
 }
